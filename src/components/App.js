f9b87584470e0a47363ce25d2a002daa28790c26
Sharonjep <jepngetichs65@gmail.com>
2023-01-25 14:22:35 -0800

    search bar effectiveness

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from'react'
 import CountButton from './CountButton/CountButton'
 import SearchBar from './SearchBar/SearchBar'
 
+const products =[
+  "tooth paste",
+  "tooth brush",
+  "mouth wash",
+  "dental floss",
+  "mouth guard",
+]
+
 
 const App = ()=>{
   
   return(
     <div>
-       <SearchBar/>
+       <SearchBar searchItems ={products}/>
     </div>
 
   )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBar.css'
+ const  products=[
+    "tooth paste",
+    "tooth brush",
+    "mouth wash",
+    "dental floss",
+    "mouth guard",
+ ]
 
-const SearchBar =()=>{
+const SearchBar =()=>{ 
+    const [searchValue, setSearchValue] = useState("the search value")
+
+    const handleInputChange = (event)=>{
+        setSearchValue(event.target.value)
+    }
+    const handleClearClick=() =>{
+        setSearchValue("")
+    }
+    const shouldDisplayButton = searchValue.length> 0
+
+    const filteredProducts = products.filter((product)=>{
+        return product.includes(searchValue)
+    }
+    )
+    
+
+
+    
+
     return (
         <div>
-            <input type="text"/>
+            <input type="text" value={searchValue} onChange={handleInputChange}/>
+            {shouldDisplayButton && <button onClick={handleClearClick}> clear </button>}
+
+            <ul>
+                {
+                    filteredProducts.map((product) =>{
+                        return<li> key={product} &{product}</li>
+
+                    })}
+                
+            </ul>
+            
             
         </div>
     )
-}
+}                  
 
 
 export default SearchBar
